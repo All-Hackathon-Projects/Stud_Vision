d889f190951c5d3a1be3ecaa6cd81a82cfca0b2e
Jonathan-Xue <jonathanxue50@gmail.com>
2018-09-08 08:14:28 -0500

    Basic Image To Text

--- app/src/main/java/com/pennappsxviii/visualize/MainActivity.java
+++ app/src/main/java/com/pennappsxviii/visualize/MainActivity.java
@@ -10,21 +10,40 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.Settings;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentTextRecognizer;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
@@ -32,12 +51,15 @@ public class MainActivity extends AppCompatActivity {
     public static final String ALLOW_KEY = "ALLOWED";
     public static final String CAMERA_PREF = "camera_pref";
 
+    private TextView documentText;
     private String mCurrentPhotoPath;
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        documentText = (TextView) findViewById(R.id.documentText);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             if (getFromPref(this, ALLOW_KEY)) {
@@ -152,6 +174,22 @@ public class MainActivity extends AppCompatActivity {
         context.startActivity(i);
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",    /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
     private void openCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
@@ -174,19 +212,55 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",    /* suffix */
-                storageDir      /* directory */
-        );
+    public void textRecognition(View view) {
+        Bitmap bitmapImage = BitmapFactory.decodeFile(mCurrentPhotoPath);
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmapImage);
+        FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
 
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
+        textRecognizer.processImage(image)
+            .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                @Override
+                public void onSuccess(FirebaseVisionText result) {
+                    // Task completed successfully
+                    String resultText = result.getText();
+                    for (FirebaseVisionText.TextBlock block: result.getTextBlocks()) {
+                        String blockText = block.getText();
+                        Float blockConfidence = block.getConfidence();
+                        List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+                        Point[] blockCornerPoints = block.getCornerPoints();
+                        Rect blockFrame = block.getBoundingBox();
+                        for (FirebaseVisionText.Line line: block.getLines()) {
+                            String lineText = line.getText();
+                            Float lineConfidence = line.getConfidence();
+                            List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+                            Point[] lineCornerPoints = line.getCornerPoints();
+                            Rect lineFrame = line.getBoundingBox();
+                            for (FirebaseVisionText.Element element: line.getElements()) {
+                                String elementText = element.getText();
+                                Float elementConfidence = element.getConfidence();
+                                List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+                                Point[] elementCornerPoints = element.getCornerPoints();
+                                Rect elementFrame = element.getBoundingBox();
+                            }
+                        }
+                    }
+                    documentText.setText(resultText);
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Task failed with an exception
+                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                    alertDialog.setTitle("Alert");
+                    alertDialog.setMessage("Invalid Image");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    alertDialog.show();
+                }
+            });
     }
 }
